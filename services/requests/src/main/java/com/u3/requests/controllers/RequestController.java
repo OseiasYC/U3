d6c98bfcbdbe43ee332119e4ac3cef8042e249fd
Iago Roque <71741741+iagoroque@users.noreply.github.com>
2024-04-09 23:22:37 -0300

    Update RequestController.java

--- services/requests/src/main/java/com/u3/requests/controllers/RequestController.java
+++ services/requests/src/main/java/com/u3/requests/controllers/RequestController.java
@@ -3,6 +3,7 @@ package com.u3.requests.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.u3.requests.enums.RequestStatus;
 import com.u3.requests.models.Request;
@@ -24,38 +26,43 @@ public class RequestController {
     RequestService requestService;
 
     @GetMapping("/{requestId}")
-    public Request getRequest(@PathVariable Long requestId) {
-        return requestService.getRequest(requestId);
+    public ResponseEntity<Request> getRequest(@PathVariable Long requestId) {
+        return ResponseEntity.ok(requestService.getRequest(requestId));
     }
 
     @GetMapping("/all")
-    public List<Request> getAllRequests() {
-        return requestService.getAllRequests();
+    public ResponseEntity<List<Request>> getAllRequests() {
+        return ResponseEntity.ok(requestService.getAllRequests());
     }
 
     @GetMapping("/student/{studentRm}")
-    public List<Request> getRequestsByStudentRm(@PathVariable String studentRm) {
-        return requestService.getRequestsByStudentRm(studentRm);
+    public ResponseEntity<List<Request>> getRequestsByStudentRm(@PathVariable String studentRm) {
+        return ResponseEntity.ok(requestService.getRequestsByStudentRm(studentRm));
     }
 
     @GetMapping("/status/{status}")
-    public List<Request> getRequestsByStatus(@PathVariable RequestStatus status) {
-        return requestService.getRequestsByStatus(status);
+    public ResponseEntity<List<Request>> getRequestsByStatus(@PathVariable RequestStatus status) {
+        return ResponseEntity.ok(requestService.getRequestsByStatus(status));
     }
 
     @PostMapping("/create")
-    public void createRequest(@RequestBody Request request) {
+    public ResponseEntity<String> createRequest(@RequestBody Request request,
+            UriComponentsBuilder uriComponentsBuilder) {
         requestService.createRequest(request);
+        var uri = uriComponentsBuilder.path("/request/{id}").buildAndExpand(request.getId()).toUri();
+        return ResponseEntity.created(uri).body("Request created successfully");
     }
 
     @PutMapping("/update/{requestId}")
-    public void updateRequest(@RequestBody Request request, @PathVariable Long requestId) {
+    public ResponseEntity<String> updateRequest(@RequestBody Request request, @PathVariable Long requestId) {
         requestService.updateRequest(request);
+        return ResponseEntity.ok("Request updated successfully");
     }
 
     @DeleteMapping("/delete/{requestId}")
-    public void deleteRequest(@PathVariable Long requestId) {
+    public ResponseEntity<String> deleteRequest(@PathVariable Long requestId) {
         requestService.deleteRequest(requestId);
+        return ResponseEntity.ok("Request deleted successfully");
     }
 
 }
