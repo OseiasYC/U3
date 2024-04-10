3231b6b9c16d2a332827f041c81347798cf48674
Lucas Farias <lucas_farias6@outlook.com>
2024-04-10 20:24:44 -0300

    Requests and Library pages created

--- management/src/main/frontend/src/App.js
+++ management/src/main/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Login from './pages/LoginPage/LoginPage.jsx';
 import Home from './pages/HomePage/HomePage.jsx';
 import Header from './components/Header/Header.jsx';
@@ -12,7 +12,7 @@ import Cursos from './pages/FunctionPages/Cursos';
 // import Home from './components/Home/home-tailwind.jsx';
 
 
-function App() {
+/* function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false); // Estado de autenticação
 
   // Função para fazer login
@@ -25,32 +25,44 @@ function App() {
   // Função para fazer logout
   const handleLogout = () => {
     setIsLoggedIn(false);
-  };
+  }; */
 
+const App = () => {
   return (
-    <>
-      <Header />
-      <Router>
+    <Router>
+      <Routes>
+        <Route path="/*" element={<AppLayout />} />
+      </Routes>
+    </Router>
+  );
+};
+
+const AppLayout = () => {
+  const location = useLocation();
+
+  // Verifica se a rota atual é diferente de "/login"
+  if (location.pathname !== "/login") {
+    return (
+      <>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/dashboard" Component={Dashboard} />
-          <Route path="/biblioteca" Component={Biblioteca} />
-          <Route path="/atendimento" Component={Atendimento} />
-          <Route path="/matricula" Component={Matricula} />
-          <Route path="/avaliacao" Component={Avaliacao} />
-          <Route path="/cursos" Component={Cursos} />
-          {/* <Route path="/login" element={<Login onLogin={handleLogin} />} />
-        {isLoggedIn ? (
-          <Route path="/home" element={<Home onLogout={handleLogout} />} />
-        ) : (
-          // Se não estiver logado, redireciona para a página de login
-          <Route path="*" element={<Navigate to="/login" replace />} />
-        )} */}
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/biblioteca" element={<Biblioteca />} />
+          <Route path="/atendimento" element={<Atendimento />} />
+          <Route path="/matricula" element={<Matricula />} />
+          <Route path="/avaliacao" element={<Avaliacao />} />
+          <Route path="/cursos" element={<Cursos />} />
         </Routes>
-      </Router>
-    </>
-  );
-}
+      </>
+    );
+  } else {
+    return (
+      <Routes>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    );
+  }
+};
 
 export default App;
--- management/src/main/frontend/src/pages/FunctionPages/Atendimento.jsx
+++ management/src/main/frontend/src/pages/FunctionPages/Atendimento.jsx
@@ -1,10 +1,73 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AiOutlineFileAdd } from "react-icons/ai";
+import { BiSearchAlt } from "react-icons/bi";
+import './Atendimento.css';
+
+// TODO: Criar a lógica de lançamento ao banco de dados 
 
 const Atendimento = () => {
+  const [serviceTitle, setServiceTitle] = useState('');
+  const [requestDescription, setRequestDescription] = useState('');
+  const [registrationNumber, setRegistrationNumber] = useState('');
+  const [sendingDate] = useState(new Date().toLocaleString());
+  // Criar lógica de armazenamento de arquivo no db:
+  // const [file, setFile] = useState<File | null>(null);
+
+  const [services, setServices] = useState([
+    { id: '1', title: 'Solicitação de Trancamento de Matrícula' },
+    { id: '2', title: 'Emissão de Comprovante de Matrícula' },
+    { id: '3', title: 'Solicitação de Agendamento de Orientação Acadêmica' },
+  ]); // Simulated data for search dropdown
+
   return (
-    <div>
-      <h2>Atendimento</h2>
-      {/* Conteúdo do Atendimento */}
+    <div className='service-request-div'>
+      <h1>Atendimento</h1>
+      <div className='input-group'>
+        {/* TODO: Ajustar posição de ícones */}
+        {/* <BiSearchAlt className='icon' /> */} 
+        <input 
+          type="text" 
+          list="service-list" 
+          placeholder="Selecione o serviço"
+          value={serviceTitle}
+          onChange={(e) => setServiceTitle(e.target.value)} 
+        />
+        <datalist id="service-list">
+          {services.map((service) => (
+            <option key={service.id} value={service.title} />
+          ))}
+        </datalist>
+      </div>
+      <div className='input-group'>
+        <textarea 
+          placeholder="Descreva a solicitação"
+          value={requestDescription}
+          onChange={(e) => setRequestDescription(e.target.value)} 
+          required
+        />
+      </div>
+      <div className='input-group'>
+        <input 
+          type="text" 
+          placeholder="Insira a matrícula do(a) estudante"
+          value={registrationNumber}
+          onChange={(e) => setRegistrationNumber(e.target.value)} 
+          required
+        />
+      </div>
+      <div className='input-group'>
+        {/* TODO: Ajustar posição de ícones */}
+        {/* <AiOutlineFileAdd className='icon' /> */}
+        <input 
+          type="file" 
+          // Criar lógica de armazenamento de arquivo no db:
+          // onChange={(e) => setFile(e.target.files ? e.target.files[0] : null)} 
+        />
+      </div>
+      <div className='sending-date'>
+        <label>Data da solicitação: {sendingDate}</label>
+      </div>
+      <button>Enviar solicitação</button>
     </div>
   );
 }
