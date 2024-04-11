0069d0237d6953d86f222c8f38e57b5b3e30b184
Iago Roque <71741741+iagoroque@users.noreply.github.com>
2024-04-11 09:16:39 -0300

    delete data.sql

--- services/courses/src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
--- Courses
-INSERT INTO course (id, name, students_rm) VALUES 
-('BES', 'Bacharelado em Engenharia de Software', ARRAY['RM1', 'RM2', 'RM3']),
-('ADS', 'Análise e Desenvolvimento de Sistemas', ARRAY['RM1', 'RM2', 'RM3']);
--- Subjects
-INSERT INTO subject (name, workload) VALUES 
-('Arquitetura de Computadores', 60),
-('Gestão do Conhecimento', 60),
-('Banco de Dados I', 60);
--- services/requests/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO request (student_rm, title, description, request_date, status)
-VALUES ('200020000', 'Requerimento de Horas Complementares', 'Quero validar mais horas complementares', CURRENT_TIMESTAMP, 'OPEN');
