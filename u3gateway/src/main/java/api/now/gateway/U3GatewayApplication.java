325959b9249eb45baa5bb8f45a04717f3bca0fbb
Iago Roque <71741741+iagoroque@users.noreply.github.com>
2024-04-28 22:10:14 -0300

    Add U3ApiGateway and remove Management

---
+++ u3gateway/src/main/java/api/now/gateway/U3GatewayApplication.java
@@ -0,0 +1,12 @@
+package api.now.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U3GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U3GatewayApplication.class, args);
+	}
+}
---
+++ u3gateway/src/main/java/api/now/gateway/util/JwtUtil.java
@@ -0,0 +1,37 @@
+package api.now.gateway.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @PostConstruct
+    public void init() {
+
+    }
+
+    public Claims getAllClaimsFromToken(String token) {
+
+        return Jwts.parser().setSigningKey(this.secret.getBytes()).parseClaimsJws(token.substring(7)).getBody();
+    }
+
+    private boolean isTokenExpired(String token) {
+
+        return this.getAllClaimsFromToken(token).getExpiration().getTime() <= new Date().getTime();
+    }
+
+    public boolean isTokenValid(String token) {
+
+        return !isTokenExpired(token);
+    }
+
+}
