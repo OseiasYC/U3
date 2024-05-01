58e5409e33111e2dabe4df79b24836f05bc9a0cd
Lucas Farias <lucas_farias6@outlook.com>
2024-05-01 12:31:59 -0300

    Some fixes done

--- management-web/src/pages/FunctionPages/Biblioteca.jsx
+++ management-web/src/pages/FunctionPages/Biblioteca.jsx
@@ -7,6 +7,10 @@ const Biblioteca = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [selectedBook, setSelectedBook] = useState(null);
+  const [formData, setFormData] = useState({
+    title: "",
+  });
+  const [studentRM, setStudentRM] = useState(""); // Adiciona o estado para o número de registro do estudante
   const [typingTimeout, setTypingTimeout] = useState(null);
 
   useEffect(() => {
@@ -44,15 +48,66 @@ const Biblioteca = () => {
 
   const handleSelectBook = (selectedBook) => {
     setSelectedBook(selectedBook);
+    setFormData({
+      title: selectedBook.title // assuming you want to send only the title
+    });
   };
 
-  const handleLendBook = () => {
-    // TODO: Adicionar lógica de empréstimo relacionando ao db
-  }
+  const handleLendBook = async () => {
+    try {
+      if (!selectedBook) {
+        console.error('Nenhum livro selecionado para empréstimo');
+        return;
+      }
 
-  const handleReturnBook = () => {
-    // TODO: Adicionar lógica de devolução relacionando ao db
-  }
+      const loanData = {
+        book_id: selectedBook.id,
+        loan_date: new Date().toISOString(),
+        return_date: "", // O usuário fornecerá isso
+        loan_status: "BORROWED", // Pode ser "BORROWED" por padrão
+        student_rm: studentRM // Usa o valor de studentRM
+      };
+
+      const response = await fetch('http://localhost:8084/loans/save', {
+        method: 'POST',
+        body: JSON.stringify(loanData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      const json = await response.json();
+      console.log(response);
+      console.log(json);
+    } catch (error) {
+      console.error('Erro ao emprestar livro:', error);
+    }
+  };
+
+
+  const handleReturnBook = async () => {
+    try {
+      const returnData = {
+        book_id: selectedBook.id,
+        return_date: "", // O usuário fornecerá isso
+        loan_status: "RETURNED" // Define o status como "RETURNED" ao devolver
+      };
+
+      const response = await fetch(`http://localhost:8084/loans/update/${selectedBook.id}`, {
+        method: 'PUT',
+        body: JSON.stringify(returnData),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      });
+
+      const json = await response.json();
+      console.log(response);
+      console.log(json);
+    } catch (error) {
+      console.error('Erro ao devolver livro:', error);
+    }
+  };
 
   return (
     <div className='library-div'>
@@ -65,17 +120,35 @@ const Biblioteca = () => {
           placeholder="Pesquise por título do livro"
           value={searchTerm}
           onChange={(e) => setSearchTerm(e.target.value)}
+          onSelect={(e) => {
+            const selectedTitle = e.target.value;
+            const selectedBook = searchResults.find(result => result.title === selectedTitle);
+            if (selectedBook) {
+              handleSelectBook(selectedBook);
+            }
+          }}
         />
         <datalist id="book-list">
           {searchResults.map((result) => (
-            <option key={result.id} value={`${result.title}`} onClick={(result) => handleSelectBook(result)}><br />{`${result.author} - Disponível: ${result.amount}`}</option>
+            <option key={result.id} value={`${result.title}`} onClick={() => handleSelectBook(result)}><br />{`${result.author} - Disponível: ${result.amount}`}</option>
           ))}
         </datalist>
       </div>
-        <button onClick={handleLendBook}>Emprestar</button>
-        <button onClick={handleReturnBook}>Devolver</button>
+      {/* Adiciona o campo de entrada para o número de registro do estudante */}
+      <div className='student-input'>
+        <FaBookOpen className='student-icon' />
+        <input
+          type="text"
+          placeholder="Número de registro do estudante"
+          value={studentRM}
+          onChange={(e) => setStudentRM(e.target.value)}
+          maxLength={9} // Limita o input a 9 caracteres
+        />
+      </div>
+      <button onClick={handleLendBook}>Emprestar</button>
+      <button onClick={handleReturnBook}>Devolver</button>
     </div>
   );
-}
+};
 
 export default Biblioteca;
